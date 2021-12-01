5b3df820c31da106c449d0189dc25041e0b40792
elvia-si <elvia.sicuro@cloudreach.com>
2021-12-01 19:46:28 +0100

    solved policy permissions on s3 bucket

--- lambda/index.py
+++ lambda/index.py
@@ -1,29 +1,39 @@
-import os
 import json
 import logging
 import boto3
+import urllib.parse
 
-# s3 = boto3.resource('s3',
-#                        aws_access_key_id=os.environ['MY_AWS_KEY_ID'],
-#                        aws_secret_access_key=os.environ['MY_AWS_SECRET_ACCESS_KEY']
-#                      )
 
-s3 = boto3.resource("s3")
 
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-bucketlist = []
-for bucket in s3.buckets.all():
-         bucketlist.append(bucket.name)
-         print(bucketlist)
+# bucketlist = []
+# for bucket in s3.buckets.all():
+#          bucketlist.append(bucket.name)
+#          print(bucketlist)
 
-def handler(event, context): 
- 
-    #   for obj in bucketlist.objects.all():
-    #     print(obj.key)
+def lambda_handler(event, context): 
+    s3 = boto3.client("s3")
+
+   # Collect the bucket name from Event
+    target_bucket_name = event['Records'][0]['s3']['bucket']['name']
+
+    # RETRIEVE all existing buckets in my account
+    my_buckets_raw = s3.list_buckets()
+
+    # List the name of each bucket
+    for b in my_buckets_raw["Buckets"]:
+        print("Name of bucket : " + b["Name"])
+
+    # Check if the target bucket exists
+    for b in my_buckets_raw["Buckets"]:
+        if b["Name"] == target_bucket_name:
+            print("The bucket " + target_bucket_name + " exists!")   
+
+
     # data = s3.get_object(Bucket='my_s3_bucket', Key='main.txt')
     # contents = data['Body'].read()
     # print(contents)
@@ -31,24 +41,34 @@ def handler(event, context):
     pet_name = ''
 
     # retrieve bucket name and file_key from the S3 event
-    bucket_name = event['Records'][0]['s3']['bucket']['name']
-    file_key = event['Records'][0]['s3']['object']['key']
-    logger.info('Reading {} from {}'.format(file_key, bucket_name))
-    # get the object
-    obj = s3.get_object(Bucket=bucket_name, Key=file_key)
-    # read the object
-    data = obj.get()['Body'].read().decode('utf-8')
-    json_data = json.loads(data)
-    #lines = obj['Body'].read().split(b'\n')
-    for item in json_data:
-       logger.info(item.decode())
-       pet_name = item.name
-    logger.info(pet_name)
+    # for record in event['Records']:
+    #     bucket_name = record[0]['s3']['bucket']['name']
+    #     file_key = record[0]['s3']['object']['key']
+    #     response = s3.head_object(Bucket=bucket_name, Key=file_key)
+    #     print(response)
 
-    return {
-       'status': 'ok'
-   }
+#     bucket_name = event['Records'][0]['s3']['bucket']['name']
+#     file_key = event['Records'][0]['s3']['object']['key']
+    
+#     logger.info('Reading {} from {}'.format(file_key, bucket_name))
+#     # get the object
+#     obj = s3.get_object(Bucket=bucket_name, Key=file_key)
+#     # read the object
+#     data = obj.get()['Body'].read().decode('utf-8')
+#     json_data = json.loads(data)
+#     #lines = obj['Body'].read().split(b'\n')
+#     for item in json_data:
+#        logger.info(item.decode())
+#        pet_name = item.name
+#     logger.info(pet_name)
 
+#     return {
+#        'status': 'ok'
+#    }
+
+  
+
+    
 
 
 
--- main.tf
+++ main.tf
@@ -34,10 +34,9 @@ resource "aws_s3_bucket_object" "sample_data" {
 resource "aws_iam_role" "iam_for_lambda_practice1" {
   name = "lambda-practice1"
 
-    assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
+    assume_role_policy = jsonencode({
+  "Version" = "2012-10-17"
+  "Statement" = [
     {
       "Action": "sts:AssumeRole",
       "Principal": {
@@ -47,30 +46,34 @@ resource "aws_iam_role" "iam_for_lambda_practice1" {
       "Sid": ""
     }
   ]
-}
-EOF
+})
 }
 
 #IAM read and write policies and cloudwatch logger
 resource "aws_iam_policy" "lambda_practice1_policy" {
     name = "lambda-practice1-policy"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
+  policy = jsonencode({
+  "Version" = "2012-10-17"
+  "Statement" = [
    {
-        "Sid": "ListObjectsInBucket",
+        "Sid": "ListBuckets",
         "Effect": "Allow",
-        "Action": ["s3:ListBucket"],
-        "Resource": ["arn:aws:s3:::ta-954444250632-lambda-lab"]
+        "Action": ["s3:ListAllMyBuckets"],
+        "Resource": "*"
     },
     {
-        "Sid": "AllObjectActions",
-        "Effect": "Allow",
-        "Action": "s3:*Object",
-        "Resource": ["arn:aws:s3:::ta-954444250632-lambda-lab/*"]
-    },
+        "Sid" : "s3access",
+        "Action" : [
+          "s3:GetObject",
+          "s3:PutObject",
+        ],
+        "Effect" : "Allow",
+        "Resource" : [
+          "arn:aws:s3:::ta-954444250632-lambda-lab", 
+          "arn:aws:s3:::ta-954444250632-lambda-lab/*"
+        ]
+      },
     {
       "Action": [
         "logs:CreateLogGroup",
@@ -81,8 +84,7 @@ resource "aws_iam_policy" "lambda_practice1_policy" {
       "Resource": "*"
     }
   ]
-}
-EOF
+})
 }
 
 #Attach IAM Role and policies
@@ -91,23 +93,15 @@ resource "aws_iam_role_policy_attachment" "test-attach" {
   policy_arn = aws_iam_policy.lambda_practice1_policy.arn
 }
 
-#S3 permission to trigger Lambda 
-resource "aws_lambda_permission" "allow_bucket_trigger" {
-   statement_id = "AllowExecutionFromS3Bucket"
-   action = "lambda:InvokeFunction"
-   function_name = aws_lambda_function.s3_read_function.function_name
-   principal = "s3.amazonaws.com"
-   source_arn = "arn:aws:s3:::ta-954444250632-lambda-lab"
-}
+# #S3 permission to trigger Lambda 
+# resource "aws_lambda_permission" "allow_bucket_trigger" {
+#    statement_id = "AllowExecutionFromS3Bucket"
+#    action = "lambda:InvokeFunction"
+#    function_name = aws_lambda_function.s3_read_function.function_name
+#    principal = "s3.amazonaws.com"
+#    source_arn = "arn:aws:s3:::ta-954444250632-lambda-lab"
+# }
 
-resource "aws_lambda_permission" "allow_cloudwatch" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.s3_read_function.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = "arn:aws:events:eu-west-1:111122223333:rule/RunDaily"
-  # qualifier     = aws_lambda_alias.test_alias.name
-}
 
 #Data source to zip lambda
 data "archive_file" "my_lambda_read_function" {
@@ -120,15 +114,15 @@ data "archive_file" "my_lambda_read_function" {
 resource "aws_lambda_function" "s3_read_function" {
    filename = "lambda.zip"
    source_code_hash = data.archive_file.my_lambda_read_function.output_base64sha256
-   function_name = "s3_read_function"
+   function_name = "index"
    role = aws_iam_role.iam_for_lambda_practice1.arn
-   handler = "index.handler"
+   handler = "index.lambda_handler"
    runtime = "python3.8"
 
 #    environment {
 #        variables = {
-#            DST_BUCKET = "${var.env_name}-dst-bucket",
-#            REGION = "${var.aws_region}"
+#            BUCKET = "ta-954444250632-lambda-lab",
+#            REGION = "eu-west-1"
 #        }
 #    }
 }
